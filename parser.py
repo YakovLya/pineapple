a82ef8f813c7cad0dd9c835a4037d3734853f48a
Yakov Lyahovich <64806983+YakovLya@users.noreply.github.com>
2022-12-11 22:29:33 +0300

    update parser v2
    hotfix

---
+++ parser.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import numpy as np
+import os, re
+from tqdm import tqdm
+
+data = []
+for url in tqdm(os.listdir('./results')):
+    for file_name in os.listdir('./results/' + url):
+        file = open('./results/' + url + '/' + file_name, 'r')
+        try:
+            html = file.read()
+        except:
+            continue
+        for phone in re.findall('tel:\+?[\d\s()-]{10,}', html): # Телефоны
+            if [url, phone[4:], 'phone'] not in data:
+                data.append([url, phone[4:], 'phone'])
+        for code in re.findall('ОГРН[\s:-]+\d{13}', html): # ОГРН
+            if [url, code, 'ОГРН'] not in data:
+                data.append([url, code, 'ОГРН'])
+        for code in re.findall('ОГРНИП[\s:-]+\d{15}', html): # ОГРНИП (приравниваю к ОГРН)
+            if [url, code, 'ОГРН'] not in data:
+                data.append([url, code, 'ОГРН'])
+        for code in re.findall('ИНН[\s:-]+\d{10}', html): # ИНН
+            if [url, code, 'ИНН'] not in data:
+                data.append([url, code, 'ИНН'])
+        for mail in re.findall('mailto:[\w\d]+@[\w\d]+.\w{2,}', html): # Почта
+            if [url, mail[7:], 'email'] not in data:
+                data.append([url, mail[7:], 'email'])
+data = np.unique(data, axis=0).tolist()
+
+with open('out.txt', 'w') as f:
+    f.write(str(data))
